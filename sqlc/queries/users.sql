ad5156d71f28628a0b1cd6b1c3d9c667a680ed53
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2025-06-21 19:44:18 -0500

    feat: setup database, migrations and codegeneration with sqlc

--- sqlc/queries/users.sql
+++ sqlc/queries/users.sql
@@ -2,10 +2,8 @@
 INSERT INTO users (
   email,
   username,
-  password_hash,
-)
-VALUES ($1, $2, $3)
-RETURNING *;
+  password_hash
+) VALUES ($1, $2, $3) RETURNING *;
 
 -- name: GetUserByID :one
 SELECT * FROM users WHERE id = $1 LIMIT 1;
